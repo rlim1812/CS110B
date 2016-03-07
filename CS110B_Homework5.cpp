4fdab6e1b4bdd1188db863757887c2aef7e55324
Ryan Lim <ryanlim@ryans-mbp-4.attlocal.net>
2016-03-06 22:42:27 -0800

    5th Homework

--- CS110B_Homework5.cpp
+++ CS110B_Homework5.cpp
@@ -1,94 +1,105 @@
 /*
-Ryan Lim
-CS 110A
-Professor Giambattista
-11.10.2015
-*/
-
+ Ryan Lim
+ Professor Luttrell
+ CS 110B
+ 2/27/2016
+ Assignment: The League
+ */
 #include <iostream>
 using namespace std;
 
-void initializerFunction ();
-void processFunction (int[], int);
-void finalFunction (int[], int);
+//function prototypes
+void initialize_arrays(string[], int[], int);
+void sort_data(string[], int[], int);
+void display_data(string[], int[], int);
 
 int main()
 {
-    //output program purpose
-    cout << "This program accepts 10 integers,and outputs a list of values\n";
-    cout << "that does not include duplicates of original values.\n";
+    //declare arrays to hold team names and amount of wins
+    const int SIZE = 5;
+    string team_names[SIZE];
+    int team_wins[SIZE];
     
-    //call first function
-    initializerFunction();
+    //call function to initialize arrays
+    initialize_arrays(team_names, team_wins, SIZE);
+    
+    //call function to sort arrays
+    sort_data(team_names, team_wins, SIZE);
+    
+    //call function to output array information
+    display_data(team_names, team_wins, SIZE);
+    return 0;
 }
 
-void initializerFunction()
+void initialize_arrays(string teams[], int wins[], int array_size)
 {
-    const int size = 10;
-    int Array[size],
-          freestyle;
-    
-    for (int i = 0; i < size; i++)
+    for (int i = 0; i < array_size; i++)
     {
-        cout << "Enter a value: ";
-        cin >> freestyle;
-        Array[i] = freestyle;
+        cout << "Enter team name #" << i+1 << ": ";
+        cin >> teams[i];
+        cout << "Enter amount of wins for team #" << i+1 << ": ";
+        cin >> wins[i];
     }
-    processFunction(Array, size);
 }
 
-void processFunction(int pseudoFunction[], int num)
+void sort_data(string teams[], int wins[], int array_size)
 {
-    int count = 0;
+    string string_temp;
+    int int_temp;
+    bool swapped;
     
-    //go through array, and remove duplicates of values
-    for (int x = 0; x < num; x++)
+    do
     {
-        int baseCase = pseudoFunction[x];
-        for (int xyz = x + 1; xyz <= num; xyz++)
+        swapped = false;
+        for (int c = 0; c < array_size - 1; c++)
         {
-            if(xyz == num)
+            if (wins[c] < wins[c+1])
             {
-                break;
-            }
-            else if(pseudoFunction[xyz] == baseCase)
-            {
-                pseudoFunction[xyz] = -1;
+                //sort integer array
+                int_temp = wins[c];
+                wins[c] = wins[c+1];
+                wins[c+1] = int_temp;
+                
+                //sort string array
+                string_temp = teams[c];
+                teams[c] = teams[c+1];
+                teams[c+1] = string_temp;
+                
+                //set flag variable to true
+                swapped = true;
             }
         }
     }
-    
-    for (int hello = 0; hello < num; hello++)
-    {
-        if (pseudoFunction[hello] != -1)
-        {
-            count++;
-        }
-    }
-    
-    int finalArray[count];
-    for (int daBomb = 0; daBomb < count; daBomb++)
-    {
-        for (int helloWorld = 0; helloWorld < num; helloWorld++)
-          {
-            if (pseudoFunction[helloWorld] != - 1)
-            {
-                finalArray[daBomb] = pseudoFunction[helloWorld];
-                pseudoFunction[helloWorld] = - 1;
-                break;
-            }
-          }
-    }
-    finalFunction(finalArray, count);
+    while (swapped == true);
 }
 
-void finalFunction(int outputArray[], int outputArraySize)
+void display_data (string teams[], int wins[], int array_size)
 {
-    cout << "Here is the list of the values with duplicates removed:";
-    for (int Yoshi = 0; Yoshi < outputArraySize; Yoshi++)
+    cout << "\nLeague Standings:\n";
+    for (int d = 0; d < array_size; d++)
     {
-        cout << " ";
-        cout << outputArray[Yoshi];
+        cout << teams[d] << ": " << wins[d] << endl;
     }
-    cout << "\n";
+    cout << endl;
 }
+
+/*
+ Sample Output
+ Enter team name #1: Dodgers
+ Enter amount of wins for team #1: 92
+ Enter team name #2: Padres
+ Enter amount of wins for team #2: 74
+ Enter team name #3: Diamondbacks
+ Enter amount of wins for team #3: 79
+ Enter team name #4: Rockies
+ Enter amount of wins for team #4: 68
+ Enter team name #5: Giants
+ Enter amount of wins for team #5: 84
+ 
+ League Standings:
+ Dodgers: 92
+ Giants: 84
+ Diamondbacks: 79
+ Padres: 74
+ Rockies: 68
+ */
